7fe1e097a7581a498edd9c01276d7db65a008bea
João Pedro Soares <joao.p.soares93@gmail.com>
2025-01-31 12:42:14 +0000

    project rename

---
+++ duflix/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:duflix/app_config.dart';
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+
+final config = AppConfig(
+  watchmodeBaseUrl: Uri.parse('https://api.watchmode.com/v1'),
+  watchmodeApiKey: 'Idsv1VxxFK80sxF2ES89OlOIB2kSxmQNXj6RuPeB',
+);
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Duflix!',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
